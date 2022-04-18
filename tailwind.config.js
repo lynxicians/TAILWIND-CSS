1d450992c838799e4e98de3161f03ac34e3b40ad
lynxicians <ayubsinaga2019@gmail.com>
2022-04-18 09:18:42 +0700

    membuat simple landing page dari tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#831843',
+        secondary: '#64748b',
+        dark: '#be185d',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
